56f591c07667f6c7646d631de28aa4262938dbd2
Piyush Fale <piyushfale@gmail.com>
2025-11-13 13:00:03 +0530

    check 002

--- app/main.py
+++ app/main.py
@@ -11,5 +11,6 @@ def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "query": q}
 
 if __name__ == "__main__":
+    
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
