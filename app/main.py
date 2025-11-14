d239de77a5d841cf22439e77bbd153766e0c80bb
Piyush Fale <piyushfale@gmail.com>
2025-11-14 12:39:58 +0530

    change 008

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 from fastapi import FastAPI,Security
-from fastapi.middleware.cors import CORSMiddleware
-import students
+# from fastapi.middleware.cors import CORSMiddleware
+# import students
 from auth import azure_scheme
 
-app = FastAPI(
-    swagger_ui_oauth2_redirect_url='/oauth2-redirect',
-    swagger_ui_init_oauth={
-        'usePkceWithAuthorizationCodeGrant': True,
-        'clientId': 'b10ae25f-404b-472c-9c46-1083cd645716',
-        'scopes': 'api://80d4fbf6-4eaf-438e-9600-567b4441a2ca/access_as_user',
-    },
-)
+# app = FastAPI(
+#     swagger_ui_oauth2_redirect_url='/oauth2-redirect',
+#     swagger_ui_init_oauth={
+#         'usePkceWithAuthorizationCodeGrant': True,
+#         'clientId': 'b10ae25f-404b-472c-9c46-1083cd645716',
+#         'scopes': 'api://80d4fbf6-4eaf-438e-9600-567b4441a2ca/access_as_user',
+#     },
+# )
 
-app.add_middleware(CORSMiddleware,
-                   allow_origins = ["http//localhost:5173"],
-                   allow_credentials = True,
-                   allow_methods = ["*"],
-                   allow_headers = ["*"])
+app = FastAPI()
+# app.add_middleware(CORSMiddleware,
+#                    allow_origins = ["http//localhost:5173"],
+#                    allow_credentials = True,
+#                    allow_methods = ["*"],
+#                    allow_headers = ["*"])
 
 app.include_router(students.router,dependencies=[Security(azure_scheme)])
 
