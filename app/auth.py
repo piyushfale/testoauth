86e31b821dd29bfe2bc583383b7d1fa5ee1270f2
Piyush Fale <piyushfale@gmail.com>
2025-11-14 12:25:32 +0530

    modified:   app/auth.py

--- app/auth.py
+++ app/auth.py
@@ -4,4 +4,5 @@ azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
     tenant_id='0ab20ff7-b526-4a85-94b6-a7e83f5c0b9d',
     scopes={'api://80d4fbf6-4eaf-438e-9600-567b4441a2ca/access_as_user':'access_as_user'},
     allow_guest_users=True
+    
 )
